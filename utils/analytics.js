9eff027fc19d180e6988eb045dabd6b315f4e8a6
sergiodxa <sergiodxa@gmail.com>
2018-01-04 21:05:19 -0500

    Create analytics integration

---
+++ utils/analytics.js
@@ -0,0 +1,35 @@
+import ReactGA from 'react-ga';
+import env from 'utils/env';
+
+export function isLocal() {
+  return NODE_ENV !== 'production';
+}
+
+export function hasInitialized() {
+  if (typeof window !== 'undefined') {
+    return window.GA_INITIALIZED;
+  }
+  return false;
+};
+
+export function initGA() {
+  ReactGA.initialize(env.GA_ID);
+  window.GA_INITIALIZED = true;
+};
+
+export function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+export function logEvent(category = '', action = '') {
+  if (category && action) {
+    ReactGA.event({ category, action });
+  }
+};
+
+export function logException(description = '', fatal = false) {
+  if (description) {
+    ReactGA.exception({ description, fatal });
+  }
+};
