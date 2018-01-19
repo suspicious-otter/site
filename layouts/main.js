95c0506ea812b49eae7c5ca8c87b543c14bf9fcf
sergiodxa <sergiodxa@gmail.com>
2018-01-19 01:06:54 -0500

    Update main layout

--- layouts/main.js
+++ layouts/main.js
@@ -1,12 +1,10 @@
 import { Component } from "react";
+import Head from "next/head";
 import Router from "next/router";
 import NProgress from "nprogress";
 import { isLocal, hasInitialized, initGA, logPageView } from "utils/analytics";
 
-Router.onRouteChangeStart = url => {
-  console.log(`Loading: ${url}`);
-  NProgress.start();
-};
+Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
@@ -23,6 +21,27 @@ export default class extends Component {
 
   render() {
     const { children } = this.props;
-    return <main>{children}</main>;
+    return (
+      <main>
+        <Head>
+          <link rel="stylesheet" href="/static/nprogress.css" />
+        </Head>
+        {children}
+        <style jsx global>{`
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+            margin: 0;
+            font-size: 1rem;
+            font-weight: 200;
+          }
+          ::selection {
+            background-color: black;
+            color: white;
+          }
+        `}</style>
+      </main>
+    );
   }
 }
