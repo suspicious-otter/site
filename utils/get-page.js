332062fb36cc0c610554c4a7b210c28843ed7cb7
sergiodxa <sergiodxa@gmail.com>
2018-01-19 01:05:23 -0500

    Utility to get the href and as of a page based on the name and params

---
+++ utils/get-page.js
@@ -0,0 +1,56 @@
+import { stringify } from "querystring";
+import routes from "../server/routes.json";
+
+/**
+ * Remove empty keys from query
+ * @param {Object} query The query to clear
+ * @returns {Object} Cleared query
+ */
+function clearQuery(query) {
+  return Object.entries(query)
+    .filter(([, value]) => value !== "")
+    .reduce((merged, [key, value]) => ({ ...merged, [key]: value }), {});
+}
+
+/**
+ * Check if the query is not empty
+ * @param {Object} query The query to verify
+ * @returns {Boolean} If the query is not empty
+ */
+function hasQuery(query) {
+  return JSON.stringify(query) !== "{}";
+}
+
+/**
+ * Get the href and as version of a page
+ * @param {String} name The name of the page
+ * @param {Object} params The parameters of the page
+ * @param {Object} query The query to add to the page
+ */
+function getPage(name, params = {}, query = {}) {
+  const route = routes.find(route => route.name === name);
+
+  if (!route) {
+    throw new ReferenceError(`Could not found a matching page for ${name}`);
+  }
+
+  const { page, path } = route;
+
+  const href = `${page}?${stringify({ ...params, ...clearQuery(query) })}`;
+
+  const as = Object.entries(params)
+    .filter(([, value]) => value !== "")
+    .reduce(
+      (replacedPath, [key, value]) => replacedPath.replace(`:${key}`, value),
+      path
+    );
+
+  return {
+    href,
+    as: hasQuery(clearQuery(query))
+      ? `${as}?${stringify(clearQuery(query))}`
+      : as
+  };
+}
+
+export default getPage;
