6770268830298c8d87045c0d14072cc127f24f37
sergiodxa <sergiodxa@gmail.com>
2018-01-19 22:32:30 -0500

    Remove router change events

--- layouts/main.js
+++ layouts/main.js
@@ -1,12 +1,6 @@
 import { Component } from "react";
 import Head from "next/head";
-import Router from "next/router";
-import NProgress from "nprogress";
 import { isLocal, hasInitialized, initGA, logPageView } from "utils/analytics";
-
-Router.onRouteChangeStart = () => NProgress.start();
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
 
 export default class extends Component {
   componentDidMount() {
