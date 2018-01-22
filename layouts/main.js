1eabd8e1b3576a110cec15f214c0fe36f7c00218
sergiodxa <sergiodxa@gmail.com>
2018-01-21 19:43:27 -0500

    Make Main layout use Header and CSS Grid to organize the layout

--- layouts/main.js
+++ layouts/main.js
@@ -2,6 +2,9 @@ import { Component } from "react";
 import Head from "next/head";
 import { isLocal, hasInitialized, initGA, logPageView } from "utils/analytics";
 
+import Header from "components/layout/header";
+import Footer from "components/layout/footer";
+
 export default class extends Component {
   componentDidMount() {
     if (isLocal()) return;
@@ -20,7 +23,13 @@ export default class extends Component {
         <Head>
           <link rel="stylesheet" href="/static/nprogress.css" />
         </Head>
-        {children}
+
+        <Header page={props.page} />
+
+        <section id="content">{children}</section>
+
+        {/* <Footer /> */}
+
         <style jsx global>{`
           body {
             font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
@@ -33,6 +42,39 @@ export default class extends Component {
           ::selection {
             background-color: black;
             color: white;
+          }
+        `}</style>
+
+        <style jsx>{`
+          main {
+            max-width: 62em;
+            margin: 0 auto;
+
+            animation-name: load;
+            animation-duration: 1000ms;
+            display: grid;
+            min-height: 100vh;
+            grid-template-areas:
+              "header"
+              "content"
+              "footer";
+            grid-template-rows: 60px auto auto;
+            grid-column: 1;
+            grid-row: 3;
+            transition: all 300ms;
+          }
+
+          #content {
+            grid-area: content;
+          }
+
+          @keyframes load {
+            0% {
+              opacity: 0;
+            }
+            100% {
+              opacity: 1;
+            }
           }
         `}</style>
       </main>
