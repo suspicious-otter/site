ed4ceceef8baed0b4289347bc88390c70c16b5b8
sergiodxa <sergiodxa@gmail.com>
2018-01-24 00:52:48 -0500

    Create forum board basic UI

---
+++ pages/forum/board.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import Head from "next/head";
+
+import { H1, H4 } from "components/ui/heading";
+import Topic from "components/forum/board/topic";
+import Toolbar from "components/forum/toolbar";
+
+import Main from "layouts/main";
+
+import topics from "data/topics.json";
+
+class ForumBoard extends Component {
+  static async getInitialProps() {
+    return { topics };
+  }
+
+  render() {
+    return (
+      <Main page="forum-board">
+        <Head>
+          <title>EdTech - Community</title>
+        </Head>
+
+        <H1>Community forum</H1>
+
+        <Toolbar />
+
+        <section id="board">
+          {this.props.topics.map(topic => <Topic key={topic.id} {...topic} />)}
+        </section>
+      </Main>
+    );
+  }
+}
+
+export default ForumBoard;
