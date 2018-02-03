ca955190e9081e28679de5a8282d1e22f379f503
sergiodxa <sergiodxa@gmail.com>
2018-02-02 23:37:34 -0500

    Add constants to check if is server or browser

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
 export const IDLE = Symbol("IDLE");
 export const FETCHING = Symbol("FETCHING");
 export const ERROR = Symbol("ERROR");
+export const IS_SERVER = typeof window === "undefined";
+export const IS_BROWSER = !IS_SERVER;
